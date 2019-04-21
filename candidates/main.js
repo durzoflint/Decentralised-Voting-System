a2705aa1cfc1eae90d2a7d95ddf99b8feebeb6d0
durzoflint <abhinavu1201@gmail.com>
2019-04-21 19:09:34 +0530

    Code formatting

--- candidates/main.js
+++ candidates/main.js
@@ -4,16 +4,15 @@ function showCandidateDetails(id) {
 	var index = id[id.length -1];
 	var candidateId = "candidate" + index;
 
-	if (currentCandidate != "not set")
+	if (currentCandidate == "not set") {
+		currentCandidate = candidateId;
+		$("#" + currentCandidate).fadeIn("slow");
+	} else {
 		$("#" + currentCandidate).fadeOut(function() {
 			currentCandidate = candidateId;
 			$("#" + currentCandidate).fadeIn("slow");
 		});
-	else {
-		currentCandidate = candidateId;
-		$("#" + currentCandidate).fadeIn("slow");
 	}
-
 
 	/*var allCandidates = document.getElementsByClassName("candidate-info");
 	for (var i = 0; i < allCandidates.length; i++) {
