cce0a830856dad0f74427f4073ffe41cee6a5bf6
durzoflint <abhinavu1201@gmail.com>
2019-04-21 19:02:33 +0530

    Added Animations

--- candidates/main.js
+++ candidates/main.js
@@ -1,11 +1,24 @@
+var currentCandidate = "not set";
+
 function showCandidateDetails(id) {
 	var index = id[id.length -1];
 	var candidateId = "candidate" + index;
 
-	var allCandidates = document.getElementsByClassName("candidate-info");
-	for (var i = 0; i < allCandidates.length; i++) {
-		allCandidates[i].style.display = "none";
+	if (currentCandidate != "not set")
+		$("#" + currentCandidate).fadeOut(function() {
+			currentCandidate = candidateId;
+			$("#" + currentCandidate).fadeIn("slow");
+		});
+	else {
+		currentCandidate = candidateId;
+		$("#" + currentCandidate).fadeIn("slow");
 	}
 
-	document.getElementById(candidateId).style.display = "block";
+
+	/*var allCandidates = document.getElementsByClassName("candidate-info");
+	for (var i = 0; i < allCandidates.length; i++) {
+		//allCandidates[i].style.display = "none";
+	}*/
+
+	//document.getElementById(candidateId).style.display = "block";	
 }
